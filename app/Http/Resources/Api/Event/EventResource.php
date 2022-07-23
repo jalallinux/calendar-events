cd284eb42facadc1e41a636500e465f0858507cd
JalalZadeh <smjjalalzadeh93@gmail.com>
2022-07-23 12:30:05 +0430

    fix resource

--- app/Http/Resources/Api/Event/EventResource.php
+++ app/Http/Resources/Api/Event/EventResource.php
@@ -3,23 +3,23 @@
 namespace App\Http\Resources\Api\Event;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Morilog\Jalali\CalendarUtils;
 
 class EventResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray($request)
     {
         return [
-            'date' => $this->resource['date'],
-            'day' => $this->resource['day'],
+            'date' => $this->getDate($request->type),
+//            'day' => $this->resource['day'],
             'description' => $this->resource['description'],
             'additional_description' => $this->resource['additional_description'],
             'is_holiday' => $this->resource['is_holiday'],
         ];
+    }
+
+    public function getDate(string $type)
+    {
+        return $type == 'gregorian' ? CalendarUtils::createCarbonFromFormat('Y-m-d', $this->resource['date'])->format('Y-m-d') : $this->resource['date'];
     }
 }
